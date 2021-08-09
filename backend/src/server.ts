4c13dc423b84aedb798463c6d9fd25be43d500e8
Aleksandar Jovanovic <aleksandar.jovanovic@belit.co.rs>
2021-08-09 16:04:35 +0200

    backend basic setup
    started express app, created folder structure

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/PIA-PROJEKAT');
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log ('Successfully connected to MongoDB');
+})
+
+const router = express.Router();
+// add router uses later
+
+app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
